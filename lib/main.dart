fd723d619cc71292cb80b73ad82eadac3c9eaa42
daniilzirsew <daniilzirsew@gmail.com>
2025-04-22 22:32:35 +0400

    Повторни коммит с защищенными токинами

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SupabaseConnection.initializeSupabase();
   await dotenv.load(fileName: ".env");
+  await SupabaseConnection.initializeSupabase();
+
   runApp(MyApp());
 }
 
