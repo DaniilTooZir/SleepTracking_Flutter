14e71634a89ec4d0c917e3a8fd70d4c80c286cfb
daniilzirsew <daniilzirsew@gmail.com>
2025-05-09 16:39:07 +0400

    Рефакторинг логики авторизации

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/cupertino.dart';
 import 'package:sleep_tracking/models/user.dart';
 import 'package:sleep_tracking/data/database/connection_to_database.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthService {
   final SupabaseClient _client = SupabaseConnection.client;
-
+  // Метод авторизации по логину и паролю
   Future<UserModel> authorizationUser({
     required String login,
     required String password,
   }) async {
     try {
+      // Запрос к таблице Users с фильтрацией по логину и паролю
       final response =
           await _client
               .from('Users')
@@ -21,12 +21,26 @@ class AuthService {
               .maybeSingle();
 
       if (response == null) {
-        throw Exception('Пользователь не найден или неверный пароль');
+        throw AuthException('Неверный логин или пароль');
       }
-
+      // Преобразование полученных данных в модель пользователя
       return UserModel.fromMap(response);
-    } catch (e) {
-      throw Exception('Ошибка авторизации: $e');
+    }
+    on PostgrestException catch (e) {
+      throw AuthException('Ошибка базы данных: ${e.message}');
+    } on AuthException {
+      rethrow; //чтобы пробросить свои ошибки дальше
+    }
+    catch (e) {
+      throw Exception('Неизвестная ошибка при входе: $e');
     }
   }
+}
+// Кастомное исключение для ошибок авторизации
+class AuthException implements Exception {
+  final String message;
+  AuthException(this.message);
+
+  @override
+  String toString() => 'AuthException: $message';
 }
